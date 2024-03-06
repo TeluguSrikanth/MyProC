d06cb9015c3eb8df6e7a4ce912ab974b8bf41d70
TeluguSrikanth <139363665+TeluguSrikanth@users.noreply.github.com>
2024-03-06 22:49:50 +0530

    Add files via upload
    SrpingWithMAil Connection through this File

---
+++ SpringMailApplication.java
@@ -0,0 +1,33 @@
+package com.mhr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import jakarta.mail.MessagingException;
+
+@SpringBootApplication
+public class SpringMailApplication {
+
+	@Autowired
+	private EmailSenderService emailSenderService;
+	
+    @EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException{
+		emailSenderService.sendEmailWithAttachment("srikanthjava57@gmail.com",
+				 "This is email body.",
+	                "This email subject", "" +
+           "C:\\Users\\SRIKANTH\\Desktop\\Sk Resu\\Srikanth_Java_Developer_Resume_,.pdf");
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMailApplication.class, args);
+	}
+
+}
